cfcd4bc68a3abdf196aa0d2d5bb5d203b29bb488
Mpho Mphego <mpho112@gmail.com>
2019-05-24 07:27:16 +0200

    added more sleep
    Signed-off-by: Mpho Mphego <mpho112@gmail.com>

--- wifi.py
+++ wifi.py
@@ -1,5 +1,6 @@
 import network
 import utime
+import ntptime
 
 
 def wifi_connect(essid, password):
@@ -13,14 +14,13 @@ def wifi_connect(essid, password):
         # connect() appears to be async - waiting for it to complete
         while not wlan.isconnected():
             print("waiting for connection...")
-            utime.sleep(4)
-            print("checking connection...")
-        print("Wifi connect successful, network config: %s" % repr(wlan.ifconfig()))
+            utime.sleep(5)
+        print("WiFi connect successful, network config: %s" % repr(wlan.ifconfig()))
     else:
         # Note that connection info is stored in non-volatile memory. If
         # you are connected to the wrong network, do an explicity disconnect()
         # and then reconnect.
-        print("Wifi already connected, network config: %s" % repr(wlan.ifconfig()))
+        print("WiFi already connected, network config: %s" % repr(wlan.ifconfig()))
 
 
 def wifi_disconnect():
