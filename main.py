e5cf958e871619b8f53c4dbed883e80396e6ab45
Mpho Mphego <mpho112@gmail.com>
2019-05-24 08:41:38 +0200

    Getting multiple samples and force gc #I know right

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-import time
+import gc
 import machine
+import utime
 
 from utils import current_time, read_config
 
 __all__ = ["MoistureSensor"]
 
 
+def force_garbage_collect():
+    # Not so ideal but someone has to do it
+    gc.collect()
+    gc.mem_free()
+
+
 class MoistureSensor(object):
-    def __init__(self, adc_pin, config_dict, sleep=60):
+    def __init__(self, adc_pin, config_dict):
         self.adc_pin = adc_pin
         self.sensor_cal = config_dict
-        self.sleep = sleep
         self.setup_adc()
 
     def setup_adc(self):
         self.adc = machine.ADC(self.adc_pin)
 
-    @property
-    def read(self):
-        adc_value = float(self.adc.read())
-        return adc_value
+    def average(self, samples):
+        return sum(samples, 0.0) / len(samples)
+
+    def read_samples(self, n_samples=10, rate=0.5):
+        sampled_adc = []
+        for i in range(n_samples):
+            sampled_adc.append(self.adc.read())
+            utime.sleep(rate)
+        force_garbage_collect()
+        return sampled_adc
 
     def adc_map(self, current_val, fromLow, fromHigh, toLow, toHigh):
         """
@@ -57,16 +69,19 @@ class MoistureSensor(object):
 
     def run(self):
         try:
+            samples = self.read_samples()
+            print(samples)
+            sampled_adc = self.average(samples)
+            print("sampled_adc: %s" % sampled_adc)
             SoilMoistPerc = self.adc_map(
-                self.read, self.sensor_cal["wet"], self.sensor_cal["dry"], 0, 100
-            )
-            print("Soil Moisture Sensor: %s% \t %s" % (SoilMoistPerc, current_time()))
-        except Exception:
-            machine.reset()
+                sampled_adc, self.sensor_cal["wet"], self.sensor_cal["dry"], 0, 100)
+            print("Soil Moisture Sensor: %.2f%% \t %s" % (SoilMoistPerc, current_time()))
+        except Exception as exc:
+            print("Exception: %s", exc)
 
 
 if __name__ == "__main__":
     config = read_config("config.json")
-    pin_adc = 0
-    moisture_Sensor = MoistureSensor(pin_adc, config["moisture_sensor_cal"])
+    moisture_Sensor = MoistureSensor(0, config["moisture_sensor_cal"])
     moisture_Sensor.run()
+    force_garbage_collect()
