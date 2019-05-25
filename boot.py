7c5b1aac8f07f4dd79bdd6297b3357f1458eecbc
Mpho Mphego <mpho112@gmail.com>
2019-05-25 20:20:03 +0200

    Moved class to utils
    Signed-off-by: Mpho Mphego <mpho112@gmail.com>

--- boot.py
+++ boot.py
@@ -1,42 +1,10 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-import gc
 import esp
-import machine
-import utime
-
-from utils import read_config, set_tz
-from wifi import disable_wifi_ap, wifi_connect, wifi_disconnect
-
-gc.collect()
-esp.osdebug(None)
-
-__all__ = ["InitialSetUp"]
-
-
-class InitialSetUp(object):
-    def __init__(self, config_dict):
-        self.config_dict = config_dict
-
-    def setup_wifi(self, disableAP=False):
-        if disableAP:
-            disable_wifi_ap()
-
-        try:
-            print("## Connecting to WiFi")
-            wifi_connect(
-                self.config_dict["wifi_config"]["ssid"],
-                self.config_dict["wifi_config"]["password"],
-            )
-            print("## Connected to WiFi")
-        except Exception:
-            print("## Failed to connect to WiFi")
-            utime.sleep(5)
-            wifi_disconnect()
-            machine.reset()
-
+from utils import read_config, InitialSetUp
 
 if __name__ == "__main__":
+    esp.osdebug(None)
     CONFIG = read_config("config.json")
     run = InitialSetUp(CONFIG)
     run.setup_wifi()  # Connect to WIFI
-    set_tz()  # Set timezone
+    run.set_tz()  # Set timezone
