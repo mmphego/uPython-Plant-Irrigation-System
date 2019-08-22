632914ae07e4ec8484348cbf42b5af4bb88d8c2b
Mpho Mphego <mpho112@gmail.com>
2019-08-22 16:19:31 +0200

    cleanup

--- water_pump.py
+++ water_pump.py
@@ -1,35 +1,25 @@
 from machine import Pin
-from utime import sleep
-
 
 class WaterPump:
-    def __init__(self, pin, delay_pump_on=2):
+    def __init__(self, pin):
         self.pin = pin
-        self.delay_pump_on = delay_pump_on
-        self._pump = None
         self.pump_status = False
-
-    @property
-    def pump(self):
-        if not self._pump:
-            self._pump = Pin(self.pin, Pin.OUT)
-        return self._pump
+        self.pump = Pin(self.pin, Pin.OUT)
 
     def pump_on(self):
         try:
-            print("[INFO] Pump ON for %s seconds" % self.delay_pump_on)
-            self.pump.on()
-            sleep(self.delay_pump_on)
-        except Exception:
-            print("[ERROR] Failed turn Pump On!")
-        finally:
+            print("[INFO] Pump ON")
             self.pump_status = True
+            self.pump(1)
+        except Exception:
+            self.pump(0)
+            print("[ERROR] Failed turn Pump On!")
 
     def pump_off(self):
         try:
-            print("[INFO] Pump OFF after %s seconds" % self.delay_pump_on)
-            self.pump.off()
-        except Exception:
-            print("[ERROR] Failed turn Pump Off!")
-        finally:
+            print("[INFO] Pump OFF")
             self.pump_status = False
+            self.pump(0)
+        except Exception:
+            self.pump(0)
+            print("[ERROR] Failed turn Pump Off!")
